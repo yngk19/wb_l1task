019792bee3e916addb02f324fd0e596f9a49e904
Yusuf <youngeek@mail.ru>
2024-03-08 19:48:43 +0300

    t3 task done

--- t3/main.go
+++ t3/main.go
@@ -1,1 +1,24 @@
-package t3
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	// создаем слайс с элементами последовательности
+	var seq = []int{2, 4, 6, 8, 10}
+	// объявляем объект sync.WaitGroup
+	var wg sync.WaitGroup
+	// добавляем 5 воркеров
+	wg.Add(5)
+	for _, elm := range seq {
+		go func(elm int) {
+			// регистрируем завершение воркера
+			defer wg.Done()
+			fmt.Println(elm * elm)
+		}(elm)
+	}
+	// ждем завершения всех воркеров
+	wg.Wait()
+}
